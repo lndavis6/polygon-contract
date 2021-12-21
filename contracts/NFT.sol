59c1c61daa01a07a4a6f5d96e063c9fcc71db510
lndavis6 <lancendavis47@gmail.com>
2021-12-21 12:13:38 -0500

    added name

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.0;
 
 // SPDX-License-Identifier: UNLICENSED
+// Lance Davis
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
